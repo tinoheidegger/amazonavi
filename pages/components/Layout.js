3b322c96e7dbb7265b38254caca984c326cbad13
tinoheidegger <tinoheidegger83@gmail.com>
2021-09-16 11:00:19 +0000

    Add-Styles-To-Layout

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,20 +1,22 @@
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
 import Head from "next/head";
 import React from "react";
+import useStyles from "../../utils/Styles";
 
 export default function Layouts({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Amazonavi</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>amazonavi</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved. Amazonavi.</Typography>
       </footer>
     </div>
