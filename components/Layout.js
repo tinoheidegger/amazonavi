a7b1f2e0ff239461d25cef8489f43abd4e220ed3
tinoheidegger <tinoheidegger83@gmail.com>
2021-09-16 15:55:26 +0000

    Product List with DarkMode Setting

---
+++ components/Layout.js
@@ -0,0 +1,94 @@
+import React, { useContext } from "react";
+import Head from "next/head";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Container,
+  createTheme,
+  createMuiTheme,
+  CssBaseline,
+  Link,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  Switch,
+} from "@material-ui/core";
+import useStyles from "../utils/Styles";
+import { Store } from "../utils/Store";
+import Cookies from "js-cookie";
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+  const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - amazonavi` : "amazonavi"}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazonavi</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+              <NextLink href="/help" passHref>
+                <Link>Help</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Amazonani</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
